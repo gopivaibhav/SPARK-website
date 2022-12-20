db8753505cf7ad194ebbfe4c1fd483f0e0340467
dilippatnana <lcb2021030@iiitl.ac.in>
2022-12-20 23:02:13 +0530

    removed merge conflict

--- team.js
+++ team.js
@@ -16,3 +16,19 @@ $(".diamond").click(function () {
 $(".close").click(function () {
   $(".user, .userinfo, #userbg").removeClass("on");
 });
+
+// nav-bar hover animation
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) =>{
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
+
+// burger toggele button toggeling animation.
+
+const toggelButton = document.getElementsByClassName('toggle-button')[0];
+const navebarList= document.getElementsByClassName('nav-bar-list')[0]
+
+toggelButton.addEventListener('click', () => {
+    navebarList.classList.toggle('active')
+})
