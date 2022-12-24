85072d359445c44961b10881a2a1ebcbf53c5e6c
dilippatnana <lcb2021030@iiitl.ac.in>
2022-12-24 20:58:29 +0530

    Added new nav bar

--- team.js
+++ team.js
@@ -10,25 +10,9 @@ $(".diamond").click(function () {
     $diamondParent.toggleClass("on");
     clearTimeout(timeout);
   }, 300);
-  $diamondParent.next(".userinfo").toggleClass("on");
+  $diamondParent.next(".userinfo").toggleClass("on"); 
 });
 
 $(".close").click(function () {
   $(".user, .userinfo, #userbg").removeClass("on");
 });
-
-// nav-bar hover animation
-const cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', (e) =>{
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-})
-
-// burger toggele button toggeling animation.
-
-const toggelButton = document.getElementsByClassName('toggle-button')[0];
-const navebarList= document.getElementsByClassName('nav-bar-list')[0]
-
-toggelButton.addEventListener('click', () => {
-    navebarList.classList.toggle('active')
-})
